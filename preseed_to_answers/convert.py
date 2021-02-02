feca98acc2954e992f785a663270583d79383431
Dan Bungert <daniel.bungert@canonical.com>
2021-02-02 14:05:03 -0700

    Add ConversionType

--- preseed_to_answers/convert.py
+++ preseed_to_answers/convert.py
@@ -1,7 +1,32 @@
 
+from enum import Enum
+
+
+class ConversionType(Enum):
+    UnknownError = 0,
+    PassThru = 1,
+    OneToOne = 2,
+
+
 class Answer:
-    def __init__(self, line):
+    '''
+    A structure of values corresponding to how the conversion handled a given
+    input line.
+
+    Attributes
+    ----------
+    line : str
+        the output data generated from the orig_input line
+    orig_input : str
+        the input that was used to generate this Answer
+    convert_type : ConversionType
+        the type of conversion performed
+    '''
+    def __init__(self, line, orig_input, convert_type):
         self.line = line
+        self.orig_input = orig_input
+        self.convert_type = convert_type
+
 
 # assumptions:
 #  can process one line at a time
@@ -20,6 +45,7 @@ def convert(line):
     tokens = trimmed.split(' ')
     if len(tokens) > 3 and tokens[0] == 'd-i':
         locale = tokens[3]
-        return f'''Welcome:\n  lang: {locale}'''
+        output = f'''Welcome:\n  lang: {locale}'''
+        return Answer(output, line, ConversionType.OneToOne)
 
-    return line
+    return Answer(line, line, ConversionType.PassThru)
--- preseed_to_answers/tests/test_basic.py
+++ preseed_to_answers/tests/test_basic.py
@@ -1,26 +1,30 @@
 
-from convert import Answer, convert
+from convert import *
 
 
 def test_answer():
-    line = 'stuff'
-    actual = Answer(line).line
-    expected = line
-    assert expected == actual
+    orig = 'my input'
+    output = 'my output'
+    convert_type = ConversionType.PassThru
+    answer = Answer(output, orig, convert_type)
+    assert output == answer.line
+    assert orig == answer.orig_input
+    assert convert_type == answer.convert_type
 
 
 def test_di_locale():
     for locale in ['en_US', 'en_GB']:
         line = f'd-i debian-installer/locale string {locale}'
-        actual = convert(line)
+        answer = convert(line)
         expected = f'Welcome:\n  lang: {locale}'
-        assert expected == actual
+        assert expected == answer.line
+        assert ConversionType.OneToOne == answer.convert_type
 
 
 def test_di_locale_extra_stuff():
     locale = 'zz_ZZ'
     line = f' d-i debian-installer/locale string {locale} # locale comment'
-    actual = convert(line)
+    actual = convert(line).line
     expected = f'Welcome:\n  lang: {locale}'
     assert expected == actual
 
@@ -34,6 +38,6 @@ def test_comment():
         ' ' * 4,
     ]
     for line in lines:
-        actual = convert(line)
-        expected = line
-        assert expected == actual
+        answer = convert(line)
+        assert line == answer.line
+        assert ConversionType.PassThru == answer.convert_type
