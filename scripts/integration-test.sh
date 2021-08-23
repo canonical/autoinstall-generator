e81f59e6544f698df21b2c89a14dde05b77959b4
Dan Bungert <daniel.bungert@canonical.com>
2021-08-23 09:25:56 -0600

    Add integration test for unicode/parse bug

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -7,3 +7,8 @@ diff -u README.md README.md.tmp
 
 make build
 [ -f build/lib/autoinstall_generator/autoinstall-schema.json ]
+
+for cfg in test/* ; do
+    PYTHONPATH=$(realpath .) \
+        autoinstall_generator/cmd/autoinstall-generator.py $cfg -cd
+done
